39106999a0830b011ab85c5e62665e508f0bd164
Victoria West <victoriawest811@gmail.com>
2018-01-23 16:20:38 -0500

    Allow and prompted for user imput

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        //1. Allow user input of a number (guess)
+        //2. Tell user if their guess is too high or too low
+        //3. Allow 5 chances, if the number isn't guessed tell them what it is
+        //4. If the number is guessed, congratulate them
+        //5. Allow the user to play again if they would like
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your guess: ");
+        int userGuess = input.nextInt();
     }
 }
